e3e1072a30104fc883d37450d4c90996d8ae2524
Jonathan Baggaley <jbaggaley@gmail.com>
2015-10-21 06:21:12 +0100

    Added bootstrap CDN and start of BTF branding

--- appread/services/data.service.js
+++ appread/services/data.service.js
@@ -22,7 +22,8 @@
       var deferred = $q.defer();
 
       deferred.resolve({
-        solution: 'Here is the solution'
+          solution: 'It\'s alright Marty, here is the solution...',
+          img: 'http://www.slate.com/content/dam/slate/blogs/future_tense/2012/06/27/_back_to_the_future_future_day_hoax_today_not_the_date_shown_on_doc_s_delorean/1340833622481.jpg'
       });
 
       return deferred.promise;
