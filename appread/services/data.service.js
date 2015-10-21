019192ddad70a2542c874534b5442cdab7aa1e37
Ben McGeachie <info@krakendigit.al>
2015-10-21 10:10:16 +0100

    styling fluff

--- appread/services/data.service.js
+++ appread/services/data.service.js
@@ -22,8 +22,8 @@
       var deferred = $q.defer();
 
       deferred.resolve({
-          solution: 'It\'s alright Marty, here is the solution...',
-          img: 'http://www.slate.com/content/dam/slate/blogs/future_tense/2012/06/27/_back_to_the_future_future_day_hoax_today_not_the_date_shown_on_doc_s_delorean/1340833622481.jpg'
+          solution: 'Reboot the VM. Check state of API. If problem persists use Manager to unassign/re-assign the box.',
+          img: '_back_to_the_future_future_day_hoax_today_not_the_date_shown_on_doc_s_delorean/1340833622481.jpg'
       });
 
       return deferred.promise;
